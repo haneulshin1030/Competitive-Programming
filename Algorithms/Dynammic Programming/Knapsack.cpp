fad533751ed530efb31860eae2b0025cae98e4c2
skys <haneulshin1030@gmail.com>
2018-04-22 19:21:04 -0400

    Update Knapsack.cpp

--- Algorithms/Dynammic Programming/Knapsack.cpp
+++ Algorithms/Dynammic Programming/Knapsack.cpp
@@ -6,16 +6,16 @@ using namespace std;
 #define FOR(i,a) for(int i = 0; i < a; i++)
 int W, n;
 
-vector<vector<int>> m (n, vector<int> (W));
+vector<vector<int>> m (n + 1, vector<int> (W + 1));
 vector<int> v;
 vector<int> w;
 
 int knap () {
-    for (int j = 0; j < W; j++) {
+    for (int j = 0; j <= W; j++) {
         m[0][j] = 0;
     }
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < W; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
             if (w[i] > j) {
                 m[i][j] = m[i - 1][j];
             } else {
@@ -23,8 +23,11 @@ int knap () {
             }
         }
     }
+    return m[n][W];
 }
 
 int main() {
+
+
 
 }
