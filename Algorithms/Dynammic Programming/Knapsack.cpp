12874b2dab105cb3f02f3611f663b5b682aa25ce
skys <haneulshin1030@gmail.com>
2018-04-21 23:36:14 -0400

    Create Knapsack.cpp

---
+++ Algorithms/Dynammic Programming/Knapsack.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+#define FOR(i,a) for(int i = 0; i < a; i++)
+int W, n;
+
+vector<vector<int>> m (n, vector<int> (W));
+vector<int> v;
+vector<int> w;
+
+int knap () {
+    for (int j = 0; j < W; j++) {
+        m[0][j] = 0;
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < W; j++) {
+            if (w[i] > j) {
+                m[i][j] = m[i - 1][j];
+            } else {
+                m[i][j] = max (m[i - 1][j], m[i - 1][j - w[i]] + v[i]);
+            }
+        }
+    }
+}
+
+int main() {
+
+}
