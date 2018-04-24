a077ac5f0297ad7f9b8d475308c40c8be4e894fb
skys <haneulshin1030@gmail.com>
2018-04-24 19:36:50 -0400

    Update Bellman Ford.cpp

--- Algorithms/Graphs/Bellman Ford.cpp
+++ Algorithms/Graphs/Bellman Ford.cpp
@@ -5,17 +5,37 @@ using namespace std;
 #define INF 1<<30
 #define FOR(i,a) for(int i=0;i<a;i++)
 int n;
-vector<tuple<int, int, int>> edges; //from node, to node, weight
 
-int bf (int x) {
+int bf (int x, vector<vector<pair<int, int>>> &adj) {
     vector<int> dist (n, INF);
     dist[x] = 0;
-    FOR (i, n) {
-        for (auto e : edges) {
-            int a, b, w;
-            tie (a, b, w) = e; //copying edge values
-            dist[b] = min (dist[b], dist[a] + w);
+    bool nc = false; //negative cycle
+    FOR (j, n) {
+        nc = false;
+        FOR (i, n) {
+            for (auto e : adj[i]) {
+                int a = i;
+                int b, w;
+                tie (b, w) = e; //copying edge values
+
+                if (dist[b] > dist[a] + w) {
+                    dist[b] = dist[a] + w;
+                    nc = true;
+                }
+                if (!nc) {
+                    break;
+                }
+            }
         }
+    }
+    if (nc) {
+        return -1;
+    } else {
+        int ans = INF;
+        FOR (i, n) {
+            ans = min (ans, dist[i]);
+        }
+        return ans;
     }
 
 }
