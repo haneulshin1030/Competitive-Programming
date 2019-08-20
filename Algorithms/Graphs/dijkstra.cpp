e6a05d56abbe0ecff1a72038fe8f239137a26caf
Haneul Shin <haneulshin1030@gmail.com>
2019-08-19 22:24:38 -0400

    Update and rename Dijkstra.cpp to dijkstra.cpp

--- Algorithms/Graphs/dijkstra.cpp
+++ Algorithms/Graphs/dijkstra.cpp
@@ -1,16 +1,15 @@
-//for adj list
 #include<bits/stdc++.h>
 
 using namespace std;
-#define INF 1<<30
+#define INF INT_MAX
 
 int n;
 
-int dijkstra (int x, vector<vector<pair<int, int>>> &adj) {
+void dijkstra (int x, vector<vector<pair<int, int>>> &adj) {
     priority_queue<pair<int, int>> q;
     vector<bool> vis (n);
     vector<int> dist (n, INF);
-
+    
     dist[x] = 0;
     q.push ({0, x});
     while (!q.empty()) {
@@ -22,21 +21,15 @@ int dijkstra (int x, vector<vector<pair<int, int>>> &adj) {
             int b = u.first, w = u.second;
             if (dist[a] + w < dist[b]) {
                 dist[b] = dist[a] + w;
-                q.push ({ -dist[b], b});
+                q.push ({-dist[b], b});
             }
         }
     }
-    // find shortest path
-    int best = 0;
-    for (int i = 0; i < n * n; i++) {
-        best = max (best, dist[i]);
-    }
-    return best;
 }
 
-
-
 int main() {
-
-
+    cin >> n;
+    vector<vector<pair<int,int>>> adj (n);
+    //fill adj
+    dijkstra(0, adj);
 }
